703de45dd3c9b0cebf620fe7b1f3504aaf554505
Channing Liu <56740244+withtimesgo1115@users.noreply.github.com>
2020-05-28 16:53:51 +0200

    update the time calculation to compare two methods

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,30 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-void quickSort(int left, int right, vector<int>& arr){
-    if(left > right){
-        return;
-    }
-    int i, j, base, temp;
-    i = left, j = right;
-    base = arr[left];
-    while(i < j){
-        while(arr[j] >= base && i < j){
-            j--;
-        }
-        while(arr[i] <= base && i < j){
-            i++;
-        }
-        if(i < j){
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-    arr[left] = arr[i];
-    arr[i] = base;
-    quickSort(left,i-1,arr);
-    quickSort(j+1,right,arr);
+void quickSort(int left, int right, vector<int>& arr) {
+	if (left > right) {
+		return;
+	}
+	int i, j, base, temp;
+	i = left, j = right;
+	base = arr[left];
+	while (i < j) {
+		while (arr[j] >= base && i < j) {
+			j--;
+		}
+		while (arr[i] <= base && i < j) {
+			i++;
+		}
+		if (i < j) {
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+	arr[left] = arr[i];
+	arr[i] = base;
+	quickSort(left, i - 1, arr);
+	quickSort(i + 1, right, arr);
+}
+
+
+int main() {
+	vector<int> A = {6,1,2,7,9,3,4,5,10,8};
+	for (int i = 0; i < A.size(); ++i) {
+		cout << A[i] << " ";
+	}
+	cout << endl;
+	quickSort(0,A.size()-1,A);
+	for (int i = 0; i < A.size(); ++i) {
+		cout << A[i] << " ";
+	}
 }
