c576eeaee2f460a4c046a95465bbe0d5bc6278ef
Channing Liu <56740244+withtimesgo1115@users.noreply.github.com>
2020-06-15 15:40:43 +0800

    add necessary comments

--- debounce.js
+++ debounce.js
@@ -1,9 +1,13 @@
 function debounce(fn, wait){
+    // define a timer and its initial value is null
     var timeout = null;
+    // use a clousure to achieve the goal and access the timer
     return function(){
+        // if timer exists, clear current timer
         if(timeout !== null){
             clearTimeout(timeout);
         }
+        // if timer doesn't exist, set the timer again
         timeout = setTimeout(fn, wait);
     }
 }
--- throttle.js
+++ throttle.js
@@ -4,12 +4,16 @@
 // time stamp
 
 var throttle = function(func, delay){
+    // previous time when bind to the event
     var prev = Date.now();
+    // actual events happened are in the enclosure 
     return function(){
         var context = this;
         var args = arguments;
+        // current time
         var now = Date.now();
         if (now - prev >= delay){
+            // operation
             func.apply(context, args);
             prev = Date.now();
         }
@@ -26,13 +30,18 @@ window.addEventListener('scroll',throttle(handle,1000));
 
 // timer version
 var throttle2 = function(func,delay){
+    // define a empty timer
     var timer = null;
+    // actual events happened are in the enclosure 
     return function(){
         var context = this;
         var args = arguments;
+        // if the timer is null, set the timer
         if(!timer) {
             timer = setTimeout(function(){
+                // operation
                 func.apply(context, args);
+                // once we have finished the operation, clear the timer
                 timer = null;
             }, delay);
         }
@@ -48,9 +57,12 @@ window.addEventListener('scroll',throttle2(handle2,1000));
 
 // Advanced version: time stamp + timer
 var throttle3 = function(func,delay){
+    // define timer and initial time stamp
     var timer = null;
     var startTime = Date.now();
+    // use closure
     return function(){
+        // get current time when the event happened
         var curTime = Date.now();
         var remaining = delay - (curTime - startTime);
         var context = this;
