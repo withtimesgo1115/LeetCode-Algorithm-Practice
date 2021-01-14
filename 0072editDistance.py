4b1186b3240f936ba7fe2b188d638ccad5113654
Channing Liu <56740244+withtimesgo1115@users.noreply.github.com>
2021-01-14 22:43:35 +0100

    Add files via upload

---
+++ 0072editDistance.py
@@ -0,0 +1,76 @@
+'''
+给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+
+你可以对一个单词进行如下三种操作：
+
+插入一个字符
+删除一个字符
+替换一个字符
+ 
+
+示例 1：
+
+输入：word1 = "horse", word2 = "ros"
+输出：3
+解释：
+horse -> rorse (将 'h' 替换为 'r')
+rorse -> rose (删除 'r')
+rose -> ros (删除 'e')
+示例 2：
+
+输入：word1 = "intention", word2 = "execution"
+输出：5
+解释：
+intention -> inention (删除 't')
+inention -> enention (将 'i' 替换为 'e')
+enention -> exention (将 'n' 替换为 'x')
+exention -> exection (将 'n' 替换为 'c')
+exection -> execution (插入 'u')
+
+
+
+dp[i][j] 代表 word1 到 i 位置转换成 word2 到 j 位置需要最少步数
+
+所以，
+
+当 word1[i] == word2[j]，dp[i][j] = dp[i-1][j-1]；
+
+当 word1[i] != word2[j]，dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+
+其中，dp[i-1][j-1] 表示替换操作，dp[i-1][j] 表示删除操作，dp[i][j-1] 表示插入操作。
+
+注意，针对第一行，第一列要单独考虑，我们引入 '' 下图所示：
+
+第一行，是 word1 为空变成 word2 最少步数，就是插入操作
+
+第一列，是 word2 为空，需要的最少步数，就是删除操作
+'''
+
+'''
+对“dp[i-1][j-1] 表示替换操作，dp[i-1][j] 表示删除操作，dp[i][j-1] 表示插入操作。”的补充理解：
+
+以 word1 为 "horse"，word2 为 "ros"，且 dp[5][3] 为例，即要将 word1的前 5 个字符转换为 word2的前 3 个字符，也就是将 horse 转换为 ros，因此有：
+
+(1) dp[i-1][j-1]，即先将 word1 的前 4 个字符 hors 转换为 word2 的前 2 个字符 ro，然后将第五个字符 word1[4]（因为下标基数以 0 开始） 由 e 替换为 s（即替换为 word2 的第三个字符，word2[2]）
+
+(2) dp[i][j-1]，即先将 word1 的前 5 个字符 horse 转换为 word2 的前 2 个字符 ro，然后在末尾补充一个 s，即插入操作
+
+(3) dp[i-1][j]，即先将 word1 的前 4 个字符 hors 转换为 word2 的前 3 个字符 ros，然后删除 word1 的第 5 个字符
+'''
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n1 = len(word1)
+        n2 = len(word2)
+        dp = [[0] * (n2+1) for _ in range(n1+1)]
+        for j in range(1,n2+1):
+            dp[0][j] = dp[0][j-1] + 1
+        for i in range(1,n1+1):
+            dp[i][0] = dp[i-1][0] + 1
+        for i in range(1, n1+1):
+            for j in range(1, n2+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+        return dp[n1][n2]
