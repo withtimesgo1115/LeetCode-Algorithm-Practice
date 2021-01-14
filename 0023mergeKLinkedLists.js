7375d4e48036849746fe55faefd25f473ac7c3be
Channing Liu <56740244+withtimesgo1115@users.noreply.github.com>
2021-01-14 22:42:58 +0100

    Add files via upload

---
+++ 0023mergeKLinkedLists.js
@@ -0,0 +1,47 @@
+/*
+给你一个链表数组，每个链表都已经按升序排列。
+
+请你将所有链表合并到一个升序链表中，返回合并后的链表。
+
+
+示例 1：
+
+输入：lists = [[1,4,5],[1,3,4],[2,6]]
+输出：[1,1,2,3,4,4,5,6]
+解释：链表数组如下：
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+将它们合并到一个有序链表中得到。
+1->1->2->3->4->4->5->6
+示例 2：
+
+输入：lists = []
+输出：[]
+示例 3：
+
+输入：lists = [[]]
+输出：[]
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists){
+    return lists.reduce((p,n) => {
+        while(n){
+            p.push(n), n = n.next;
+        }
+        return p;
+    }, []).sort((a,b) => a.val - b.val).reduceRight((p,n) => (n.next = p, p = n, p), null);
+}
